5952ac6156303941f8227c9d9aba0a6fe405f1b5
Gunzelb <brandongunzel@gmail.com>
2021-10-11 00:04:17 -0700

    added get route to serve html

--- routes/api.js
+++ routes/api.js
@@ -1,34 +1,44 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
+const path = require("path");
 
-router.post("/api/transaction", ({body}, res) => {
+router.post("/api/transaction", ({ body }, res) => {
   Transaction.create(body)
-    .then(dbTransaction => {
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
 });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
+router.post("/api/transaction/bulk", ({ body }, res) => {
   Transaction.insertMany(body)
-    .then(dbTransaction => {
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
 });
 
 router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
+  Transaction.find({})
+    .sort({ date: -1 })
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
+});
+
+router.get("/", (req, res) => {
+  try {
+    res.status(200).sendFile(path.join(__dirname, "../public/index.html"));
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
